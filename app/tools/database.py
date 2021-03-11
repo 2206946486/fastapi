8834e6c61f7842b2ec8d84a3986d4ab75c446a4d
2206946486 <404449416@qq.com>
2021-03-11 14:07:24 +0800

    sql增加无效连接检测

--- app/tools/database.py
+++ app/tools/database.py
@@ -19,7 +19,9 @@ logger.info(f"开始加载数据库")
 
 
 engine = create_engine(
-    config.SQLALCHEMY_DATABASE_URI
+    config.SQLALCHEMY_DATABASE_URI,
+    pool_recycle=3600,
+    pool_pre_ping=True
 )
 #
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
